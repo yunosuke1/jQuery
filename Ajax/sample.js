e55146eecdd378270c79cd3759fc9e75bbd2123a
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-21 13:29:32 +0900

    Ajax作成

---
+++ Ajax/sample.js
@@ -0,0 +1,21 @@
+'use strict'
+$(function(){
+    $(document).on('click','#get_address_btn',function(){
+        $.ajax({
+            url:'https://zipcoda.net/api',
+            dataType:"jsonp",
+            data:{
+                zipcode:$('#zipcode').val(),
+            },
+            async:true
+        }).done(function(data){
+            console.dir(JSON.stringify(data));
+            $('#address').val(data.items[0].address);
+        })
+    }).fail(function(XMLHttpRequest,textStatus,errorThrown){
+        alert('正しい結果を得られませんでした');
+        console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+        console.log("textStatus : "+textStatus.textStatus);
+        console.log("errorThrown : "+errorThrown.message);
+    })
+})
